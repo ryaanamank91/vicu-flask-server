e7ff0919051d71dee940a89b4d714e6fc58b6eb7
ryaanamank91 <ryaanamank91@gmail.com>
2025-05-02 12:21:17 +0200

    Initial commit

---
+++ flask_server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+latest_data = {}
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    global latest_data
+    incoming = request.json
+    print("Received data:", incoming)
+    latest_data.update(incoming)  # <-- Fix: update dict in-place
+    return jsonify({'status': 'success'}), 200
+
+
+@app.route('/latest', methods=['GET'])
+def get_latest_data():
+    return jsonify(latest_data), 200
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
